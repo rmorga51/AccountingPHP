a6f4d329b77a78ea34633b2a90acfc9992610ecc
rmorga51 <rmorgan1262@gmail.com>
2018-04-21 19:39:39 -0400

    connection test

--- src/connect.php
+++ src/connect.php
@@ -1,4 +1,18 @@
-<?php 
+/*<?php 
 $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "accountinganywhere-mysqldbserver.mysql.database.azure.com", "mysqldbuser@accountinganywhere-mysqldbserver", Password_1, accounting, 3306);
 //echo $mysqli->host_info . "\n";
+?>*/
+
+<?php
+$config['db'] = array(
+	'host'			=>'accountinganywhere-mysqldbserver.mysql.database.azure.com',
+	'username'		=>'mysqldbuser@accountinganywhere-mysqldbserver',
+	'password'		=>'Password_1',
+	'dbname'		=>'accounting'
+);
+// set dbname to accounting before commit	
+
+$db = new PDO('mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['dbname'], $config['db']['username'], $config['db']['password']); 
+$db->setATTRIBUTE(PDO::ATTR_EMULATE_PREPARES, false);
+$db->setATTRIBUTE(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
