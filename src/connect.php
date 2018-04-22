b09168d840711bb3f7c3181c1524f44ae4228feb
rmorga51 <rmorgan1262@gmail.com>
2018-04-21 20:31:20 -0400

    test

--- src/connect.php
+++ src/connect.php
@@ -1,6 +1,6 @@
 <?php 
-$mysqli = new mysqli("accountinganywhere-mysqldbserver.mysql.database.azure.com", "mysqldbuser@accountinganywhere-mysqldbserver", "Password_1", "accounting");
-if ($mysqli->connect_errno) {
+$con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "accountinganywhere-mysqldbserver.mysql.database.azure.com", "mysqldbuser@accountinganywhere-mysqldbserver", Password_1, accounting, 3306);
+if (con->connect_errno) {
     die('Connect Error: ' . $mysqli->connect_errno);
 }
 else{
@@ -8,3 +8,4 @@ else{
 }
 //echo $mysqli->host_info . "\n";
 ?>
+
