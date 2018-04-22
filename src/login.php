b6f55e4ffa4243e1fa9cbbebd7192725a3f4af48
rmorga51 <rmorgan1262@gmail.com>
2018-04-22 00:22:19 -0400

    testing login.php

--- src/login.php
+++ src/login.php
@@ -30,7 +30,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         // Prepare a select statement
         $sql = "SELECT username, password FROM users WHERE username = :username";
         
-        if($stmt = $db->prepare($sql)){
+        if($stmt = $conn->prepare($sql)){
             // Bind variables to the prepared statement as parameters
             $stmt->bindParam(':username', $param_username, PDO::PARAM_STR);
             
