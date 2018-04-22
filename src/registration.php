15f1657eff61cf7bf36bd8c6e6273e96ea64aea4
rmorga51 <rmorgan1262@gmail.com>
2018-04-22 16:27:23 -0400

    checking connection

--- src/registration.php
+++ src/registration.php
@@ -92,6 +92,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if($stmt->execute()){
                 // Redirect to login page
+                echo 'made it here';
                 header("location: login.php");
             } else{
                 echo "Something went wrong. Please try again later.";
