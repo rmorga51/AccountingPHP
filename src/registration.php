93cd956e09b13314f16bf6d3e2a02c4332e2d922
rmorga51 <rmorgan1262@gmail.com>
2018-04-22 11:09:32 -0400

    testing connection

--- src/registration.php
+++ src/registration.php
@@ -84,7 +84,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password, user_type) VALUES ('"username,"' '"password,"' '"user_type )";
+        $sql = "INSERT INTO users (username, password, user_type) VALUES (:username, :password, :user_type )";
          
         if($stmt = $conn->prepare($sql)){
             // Bind variables to the prepared statement as parameters
