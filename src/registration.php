84cbbb97beca9e25d52d8134465a470ee748fa0b
rmorga51 <rmorgan1262@gmail.com>
2018-04-22 00:23:47 -0400

    testing registration.php connectivity

--- src/registration.php
+++ src/registration.php
@@ -1,7 +1,25 @@
 <?php
 // Include config file
 // PHP Data Objects(PDO) Sample Code:
-include("connect.php");
+
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:anywhereaccounting1server.database.windows.net,1433; Database = accounting", "rmorga51", "Password_1");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "rmorga51@anywhereaccounting1server", "pwd" => "Password_1", "Database" => "accounting", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:anywhereaccounting1server.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+
+
+
 
  
 // Define variables and initialize with empty values
