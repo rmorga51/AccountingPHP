86b3c8c8e1d058949f3f4bff08143db578d8609d
rmorga51 <rmorgan1262@gmail.com>
2018-04-21 20:18:51 -0400

    testing

--- src/connect.php
+++ src/connect.php
@@ -1,7 +1,7 @@
 <?php 
 $mysqli = new mysqli("accountinganywhere-mysqldbserver.mysql.database.azure.com", "mysqldbuser@accountinganywhere-mysqldbserver", "Password_1", "accounting");
 if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    die('Connect Error: ' . $mysqli->connect_errno);
 }
 else{
    echo 'Try harder man'; 
